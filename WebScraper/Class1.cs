a69868a60d471e0679a9fed8e8db6427ee0736b1
bobmack <bobbymaceneaney@gmail.com>
2017-04-12 23:28:52 +0100

    Add project files.

---
+++ WebScraper/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper
+{
+    public interface iScraper
+    {
+        string RegexPattern { get; }
+        string Url { get; }
+    }
+
+    public class Scraper : iScraper
+    {
+        public string RegexPattern { get; set; }
+        public string Url { get; set; }
+        
+        public string GetHtml()
+        {
+            return new WebClient().DownloadString(Url);
+        }
+        
+
+
+    }
+}
